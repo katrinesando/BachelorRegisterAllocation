189d76a5df1cdde4d7761bfe3d1e4eb3cfcd6c82
katrinesando <95019434+katrinesando@users.noreply.github.com>
2024-02-27 14:56:52 +0100

    simple array arithmetic example

--- RegisterAllocation/RegisterAllocation/Assembly/mcexamples/simpleArr.c
+++ RegisterAllocation/RegisterAllocation/Assembly/mcexamples/simpleArr.c
@@ -1,9 +1,11 @@
-int num;
-int i;
-int h;
+//local simpel arithmetic
 void main(int n)
 {
-   num = 3;
-   i = 5;
-   print i; //prøv at lave program der printer n
+   int arr[5];
+   arr[0]=1;
+   *(arr+1) = 3;
+   print *(arr+1);
+   print arr[0];
 }
+//should print 3 1
+//prints 3 769
