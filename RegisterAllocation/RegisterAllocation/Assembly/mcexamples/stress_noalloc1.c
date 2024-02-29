317546b78ecc3afe8558b52f5f6377f1ced5e063
Adam Hadou Temsamani <94063688+AdamHadouTemsamani@users.noreply.github.com>
2024-02-29 11:39:31 +0100

    Create stress_noalloc1.c

---
+++ RegisterAllocation/RegisterAllocation/Assembly/mcexamples/stress_noalloc1.c
@@ -0,0 +1,9 @@
+//This code stress tests the compiler. It should not compile if there is no register allocation.
+//Which is the case as of writting the code. 
+
+void main() {
+    int n;
+    n = 20; 
+    int complex;
+    complex = n + (n + (n + (n + (n + (n + (n + (n + (n + (n + (n + (n + n))))))))))); //Too complex of an expression
+}
