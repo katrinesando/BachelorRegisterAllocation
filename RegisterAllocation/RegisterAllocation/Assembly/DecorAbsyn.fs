58a7de4e1dc0eb7825a2bb7d60a836dfb74f30e7
Maxime Havez <56996234+Omonom47@users.noreply.github.com>
2024-04-03 15:36:03 +0200

    Expands DecorAbsyn to include exprs and access

--- RegisterAllocation/RegisterAllocation/Assembly/DecorAbsyn.fs
+++ RegisterAllocation/RegisterAllocation/Assembly/DecorAbsyn.fs
@@ -10,7 +10,21 @@ open Microsoft.VisualBasic
  *)
 open Absyn
 
-type dstmt<'info> =                                                     
+type dexpr<'info> =                                                         
+  | DAccess of daccess<'info> * 'info                (* x    or  *p    or  a[e]     *)
+  | DAssign of daccess<'info> * dexpr<'info> * 'info         (* x=e  or  *p=e  or  a[e]=e   *)
+  | DAddr of daccess<'info> * 'info                  (* &x   or  &*p   or  &a[e]    *)
+  | DCstI of int * 'info                     (* Constant                    *)
+  | DPrim1 of string * dexpr<'info> * 'info         (* Unary primitive operator    *)
+  | DPrim2 of string * dexpr<'info> * dexpr<'info> * 'info  (* Binary primitive operator   *)
+  | DAndalso of dexpr<'info>  * dexpr<'info> * 'info      (* Sequential and              *)
+  | DOrelse of dexpr<'info>  * dexpr<'info> * 'info          (* Sequential or               *)
+  | DCall of string * dexpr<'info>  list * 'info     (* Function call f(...)        *)                                                                
+and daccess<'info> =                                                       
+  | DAccVar of string * 'info           (* Variable access        x    *) 
+  | DAccDeref of dexpr<'info>                  (* Pointer dereferencing  *p   *)
+  | DAccIndex of daccess<'info> * dexpr<'info> * 'info       (* Array indexing         a[e] *)
+and dstmt<'info> =                                                     
   | DIf of expr * dstmt<'info> * dstmt<'info> * 'info           (* Conditional                 *)
   | DWhile of expr * dstmt<'info> * 'info                (* While loop                  *)
   | DExpr of expr * 'info                        (* Expression statement   e;   *)
