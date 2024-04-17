f7209861d7c57f7716616bcd74b3f0532bd78bdf
katrinesando <95019434+katrinesando@users.noreply.github.com>
2024-04-17 13:56:40 +0200

    Began fixing arrayIndexes
    currently displays the wrong for when printing an array index

--- RegisterAllocation/RegisterAllocation/Assembly/mcexamples/simpleArr.c
+++ RegisterAllocation/RegisterAllocation/Assembly/mcexamples/simpleArr.c
@@ -2,12 +2,13 @@
 void main()
 {
    int arr[5];
-   arr[0]=1;
-   *(arr+1) = 3;
-   print arr[1];
-   print arr[0];
-   *arr = 198;
-   print arr[0];
+   arr[0]=3;
+   print (arr[0]);
+   
+   //arr[1]=2;
+   //*(arr+1) = 3;
+   //print arr[1];
+   /* *arr = 198;
+   print arr[0];*/
 }
 //should print 3 1
-//prints 3 769
