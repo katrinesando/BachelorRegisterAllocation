d548032482019ce858f4421324926e9a21a9391c
katrinesando <95019434+katrinesando@users.noreply.github.com>
2024-05-01 10:45:36 +0200

    test cases added

--- RegisterAllocation/RegisterAllocation/Assembly/mcexamples/ex_medianArr.c
+++ RegisterAllocation/RegisterAllocation/Assembly/mcexamples/ex_medianArr.c
@@ -1,31 +1,41 @@
 // micro-C example 11 
 
 //find median of arrays
-
-void main() {
-	int arr1[100];
-	int arr2[100];
+void main(int num) {
+	
 	int n1;
 	int n2;
-	arr1[1] = 1;
-	arr1[2] = 12;
-	arr1[3] = 15;
-	arr1[4] = 26;
-	arr1[5] = 38;
-    
-	arr2[1] = 2;
-	arr2[2] = 13;
-	arr2[3] = 17;
-	arr2[4] = 30;
-	arr2[5] = 45;
-
+	int arr1[100];
+	int arr2[100];
+	arr1[0] = 1;
+	arr1[1] = 12;
+	arr1[2] = 15;
+	arr1[3] = 26;
+	arr1[4] = 38;
+  
+	arr2[0] = 2;
+	arr2[1] = 13;
+	arr2[2] = 17;
+	arr2[3] = 30;
+	arr2[4] = 45;
+	
 	n1 = 5;
 	n2 = 5/2;
-
-	if (n1 == n2)
-	{
-		printi (median(arr1, arr2, n1));
-	}
+	// n2 = 0;
+	// while (n2 <= 100) {
+	// 	arr1[n2] = n2;
+	// 	n2 = n2+1;
+	// }
+	// n2 = 0;
+	// n1 = 3;
+	// while (n2 <= 100) {
+	// 	n1 = n2*n1;
+	// 	arr2[n2] = n1;
+	// 	n2 = n2+1;
+	// }
+	// n1 = 100;
+	print (median(arr1, arr2, n1));
+	
 }
 
 int median(int a1[], int a2[], int n)
@@ -54,14 +64,14 @@ int median(int a1[], int a2[], int n)
 		{
 			median1 = median2; //store prev med
 			median2 = a1[i];
-			i++;
+			i = i+1;
 		}else
 		{
 			median1 = median2; //store prev med
 			median2 = a2[j];
-			j++;
+			j = j+1;
 		}
-		count++;
+		count = count +1;
 	}
 	return (median1 + median2) /2;
 }
