f878f3ce6cde8c4eddf3d839fad27583281b0dd0
katrinesando <95019434+katrinesando@users.noreply.github.com>
2024-05-02 14:50:04 +0200

    added Test and updated Readme

---
+++ RegisterAllocation/RegisterAllocation/Assembly/mcexamples/jakobTest.c
@@ -0,0 +1,107 @@
+﻿
+void main()
+{
+    int profetien;
+    profetien = oedelaeggelse();
+    print profetien;
+}
+
+
+int ondskab()
+{
+    int n;
+    n = 5;
+
+    int t;
+    t = 84 ;
+
+    int g;
+    int r;
+    int u;
+    int p;
+    int a;
+    int b;
+    int o;
+
+    g = t+n;
+
+    return n;
+}
+
+int oedelaeggelse()
+{
+    int mord;
+    int frygt;
+    int gnislen;
+    int arson;
+    int straf;
+    int torne;
+    int pisk;
+    int rotationals;
+    int doedsfascisme;
+    int stalin;
+    int megadoed;
+    int ragnarok;
+    int kollaps;
+    int henrettelse;
+    int ofring;
+    int pinsel;
+    int tortur;
+    int krig;
+    int supernova;
+    int afstraffelse;
+    int ulighed;
+    int doed;
+    doed = 1000;
+    
+    while(doed>0)
+    {
+        mord = 1;
+        frygt = mord;
+        gnislen = frygt+mord-1+1-1;
+        arson = 1;
+        straf = 1;
+        torne = frygt;       
+        pisk = mord;        
+        rotationals = pisk; 
+        doedsfascisme = torne + pisk -arson;
+        stalin = 1;      
+        megadoed = 1;     
+        ragnarok = 1;    
+        kollaps = 1;     
+        henrettelse = 1; 
+        ofring = 1;      
+        pinsel = 1;      
+        tortur = 1;      
+        krig = 1;        
+        supernova = 1;   
+        afstraffelse = 1;
+        ulighed = 1;
+        
+        ulighed = 1 - ulighed + afstraffelse - supernova + krig - tortur + pinsel - pinsel + pinsel - pinsel + pinsel - ofring + henrettelse - kollaps + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - ragnarok + ragnarok -ragnarok + ragnarok - ragnarok + ragnarok - megadoed + stalin - doedsfascisme + rotationals - pisk + torne - straf + arson - gnislen + frygt - mord + stalin;
+
+        
+        if(doed>50 && doed<75)
+        {
+            int dommedag;
+            dommedag = 10000;
+            while(dommedag>0)
+            {
+                int skraek;
+                skraek = ondskab();
+                skraek = ondskab();
+                skraek = ondskab();
+                skraek = skraek;
+                if(1==1)
+                {
+                    dommedag = (dommedag+dommedag+dommedag+dommedag+dommedag+dommedag+dommedag+dommedag+dommedag+dommedag+dommedag + skraek - skraek+ skraek - skraek - (dommedag*10));
+                }
+                dommedag = dommedag-1+1-1+1-1+1-1+1-1+1-1+1-1+1-1;
+
+                skraek = ulighed;
+            }
+        }
+        doed = doed -1;
+    }
+    return 0;    
+}
